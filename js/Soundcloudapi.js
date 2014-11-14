b8f65578c7cc59f1543e561808ff1bc6b18262a9
Alex Qin <alexandraqin@gmail.com>
2014-11-14 18:55:08 +0000

    Clean up soundcloudapi.js

--- js/Soundcloudapi.js
+++ js/Soundcloudapi.js
@@ -1,4 +1,7 @@
-var currentPlayingSoing;
+//https://soundcloud-c9-jasondecastro.c9.io/#
+//https://preview.c9.io/jasondecastro/soundcloud/index.html
+
+var currentPlayingSong;
 
 SC.initialize({
   client_id: '53e1b5c5885286f94a2f2dbe52932379',
@@ -6,54 +9,44 @@ SC.initialize({
 });
 
 // initiate auth popup
-SC.connect(function() {
+SC.connect(function () {
   SC.get('/me', function(me) { 
-   console.log(me.username);
-   $('.text-muted').html('Hello, '+me.username);
+    $('.text-muted').html('Hello, ' + me.username);
   });
 });
 
+function playTrack (trackid) {
+  if(currentPlayingSong){
+    currentPlayingSong.stop();
+  }
+  SC.stream('/tracks/' + trackid, function (sound) {
+    currentPlayingSong = sound;
+    currentPlayingSong.play();
+  });
+}
 
-
-function playTrack(trackid){
-    if(currentPlayingSoing){
-        currentPlayingSoing.stop();
-    }
-    SC.stream("/tracks/"+trackid, function(sound){
-        currentPlayingSoing=sound;
-       currentPlayingSoing.play();
-       
+function searchTracks (song) {
+  SC.get('/tracks', { q: song }, function (tracks) {
+  var songs = tracks;
+  var randomSongNumber = Math.floor(Math.random() * (songs.length - 1));
+  var song = songs[randomSongNumber];
+  playTrack(song.id);
+  $('#songtitle').html(song.user.username + " " + song.title);
 });
 }
 
-function searchTracks(song){
-    SC.get('/tracks', { q: song }, function(tracks) {
-   var songs=tracks;
-     var randomSongNumber = Math.floor(Math.random()*(songs.length - 1));
-     var song=songs[randomSongNumber];
-   playTrack(song.id);
-     console.log(song);
-     $('#songtitle').html(song.user.username + " " + song.title);
-    //
+$('#rndbtn').on('click', function () {
+  var mood = giveMeRandomWord();
+  changeColor(getColor(mood));
+  search_flickr(mood);
+  $('#moodstatus').html('It sounds like you are in a ' + mood + ' mood!!');
+  searchTracks(mood);
 });
-}
 
-//https://soundcloud-c9-jasondecastro.c9.io/#
-//https://preview.c9.io/jasondecastro/soundcloud/index.html
-
-$('#rndbtn').on('click',function (){
-    var mood=giveMeRandomWord();
-    changeColor(getColor(mood));
-                    search_flickr(mood);
-    $('#moodstatus').html('It sounds like you are in a '+ mood +' mood!!');
-    searchTracks(mood);
-    
-} );
-
-$('#go').on('click',function() {
-    var mood=$('#mood').val();
-        changeColor(getColor(mood));
-                    search_flickr(mood);
-    $('#moodstatus').html('It sounds like you are in a '+ mood +' mood!!');
-    searchTracks(mood);
+$('#go').on('click', function () {
+  var mood=$('#mood').val();
+  changeColor(getColor(mood));
+  search_flickr(mood);
+  $('#moodstatus').html('It sounds like you are in a ' + mood +  ' mood!!');
+  searchTracks(mood);
 });
